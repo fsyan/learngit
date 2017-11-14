4d2a2025d02b02960be3110d06bedf17ed0df329
fsyan <fsy19881203@163.com>
2017-11-14 12:36:58 +0800

    master

--- app/src/main/java/com/fsy/demo/MainActivity.java
+++ app/src/main/java/com/fsy/demo/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends BaseActivity {
         LayoutInflater LayoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 
-        //HelloWorld
+        //HelloWorld   分支master
     }
 
     @Override
